e1df835c4ac735d1cb3b100833f37b941816ab1f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-05 09:44:32 -0300

    New

--- BinaryTree/src/fiap/com/br/test/MainTest.java
+++ BinaryTree/src/fiap/com/br/test/MainTest.java
@@ -7,12 +7,20 @@ public class MainTest {
     public static void main(String[] args) {
         BinaryTreeV2 tree2 = new BinaryTreeV2();
 
-        tree2.init(30);
-        tree2.inserir(tree2.root ,20);
-        tree2.inserir(tree2.root ,25);
-        tree2.inserir(tree2.root ,40);
+        tree2.init(20);
+        tree2.inserir(tree2.root ,15);
+        tree2.inserir(tree2.root ,10);
+        tree2.inserir(tree2.root ,35);
+        tree2.inserir(tree2.root, 30);
         tree2.listaEmOrdem(tree2.root);
 
         System.out.println("Quantidade de nos da ABB " + tree2.contaNos(tree2.root, 0));
+
+        if(tree2.consulta(tree2.root,40)){
+            System.out.println("tem na ABB");
+        }else{
+            System.out.println("Não tem na ABB");
+        }
+        System.out.println(tree2.contaConsulta(tree2.root,30, 0));
     }
 }
--- BinaryTree/src/fiap/com/br/tree/BinaryTreeV2.java
+++ BinaryTree/src/fiap/com/br/tree/BinaryTreeV2.java
@@ -2,42 +2,78 @@ package fiap.com.br.tree;
 
 public class BinaryTreeV2 {
 
-    private class ARVORE{
+    private class ARVORE {
         int dado;
-        ARVORE dir,esq;
+        ARVORE dir, esq;
 
     }
+
     public ARVORE root = null;
-public void init(int info){
-    root = new ARVORE();
-    root.dado = info;
-}
-    public ARVORE inserir(ARVORE p , int info){
-        if(p == null){
+
+    public void init(int info) {
+        root = new ARVORE();
+        root.dado = info;
+    }
+
+    public ARVORE inserir(ARVORE p, int info) {
+        if (p == null) {
             p = new ARVORE();
             p.dado = info;
             p.esq = null;
             p.dir = null;
-        }else if (info < p.dado)
+        } else if (info < p.dado)
             p.esq = inserir(p.esq, info);
         else
             p.dir = inserir(p.dir, info);
         return p;
     }
-  public void listaEmOrdem(ARVORE p){
-        if (p!=null){
+
+    public void listaEmOrdem(ARVORE p) {
+        if (p != null) {
             listaEmOrdem(p.esq);
             System.out.println("Dado: " + p.dado);
             listaEmOrdem(p.dir);
         }
-  }
- public int contaNos(ARVORE p, int cont){
-        if(p!=null){
+    }
+
+    public int contaNos(ARVORE p, int cont) {
+        if (p != null) {
             cont++;
             cont = contaNos(p.esq, cont);
             cont = contaNos(p.dir, cont);
         }
-      return cont;
- }
+        return cont;
+    }
+
+    public boolean consulta(ARVORE p, int info) {
+        if (p == null) {
+            return false;
+        } else {
+            if (p.dado == info) {
+                return true;
+            } else {
+                if (info < p.dado) {
+                    return consulta(p.esq, info);
+                } else {
+                    return consulta(p.dir, info);
+                }
+            }
+        }
+    }
+
+    public int contaConsulta(ARVORE p, int info, int cont) {
+        if (p != null) {
+            cont++;
+            if (p.dado == info)
+                return cont;
+         else {
+            if (p.dado < info)
+                cont = contaConsulta(p.esq, info, cont);
+             else
+                 cont = contaConsulta(p.dir, info, cont);
+            }
+        }
+        return cont;
+    }
 
 }
